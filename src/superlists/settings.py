ce5834c0224c9ec328f5b778ba53ee3dc34f9672
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2024-05-09 11:56:38 -0300

    Moved static folder and Fts into their own individual files

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if "DJANGO_DEBUG_FALSE" in os.environ:  
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]  
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True  
+    SECRET_KEY = "insecure-key-for-dev"
+    ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -124,3 +133,14 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "root": {"handlers": ["console"], "level": "INFO"},
+    },
+}
